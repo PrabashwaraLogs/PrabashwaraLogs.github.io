7e7bcc9e2631da7d15bbf02885af1c109b888e44
Rivibibu Prabshwara Ⓒ <96346708+PrabashwaraX@users.noreply.github.com>
2021-12-29 18:09:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+/* MENU SHOW */ 
+/* CopyRight to @WKRPrabashwara */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+
+/*----- ANIMATE -----*/
+// OVERLAY
+
+// IMG
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
+
+// INFORMATION
+gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
+
+// NAV ITEM
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
+
+// SOCIAL
+gsap.from('.socialiconlol', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.text-box', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.about__text', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.inner', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.container', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
+
+gsap.from('.hww', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
